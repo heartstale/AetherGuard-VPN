fb33aa8c181946c41c089e7e8b72d06119fc5c2a
heartstale <babymngm@gmail.com>
2026-02-24 10:27:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,59 @@
 const btn = document.getElementById("connectBtn");
-const progressBar = document.getElementById("progressBar");
-const statusText = document.getElementById("statusText");
-const statusDot = document.getElementById("statusDot");
-const locationSelect = document.getElementById("locationSelect");
-const locationText = document.getElementById("locationText");
+const indicator = document.getElementById("indicator");
+const statusTitle = document.getElementById("statusTitle");
+const statusSub = document.getElementById("statusSub");
+const circle = document.querySelector(".circle");
+const progressText = document.getElementById("progressText");
 const ipText = document.getElementById("ipText");
 const pingText = document.getElementById("pingText");
+const locationSelect = document.getElementById("locationSelect");
+const locationText = document.getElementById("locationText");
 const loveScreen = document.getElementById("loveScreen");
 const app = document.getElementById("app");
 
 btn.addEventListener("click", () => {
 
   btn.disabled = true;
-  statusText.textContent = "Connecting...";
-  statusDot.style.background = "orange";
+  statusTitle.textContent = "Идёт подключение...";
+  statusSub.textContent = "Устанавливаем соединение с сервером";
+  indicator.style.background = "orange";
 
   locationText.textContent = locationSelect.value;
 
   let progress = 0;
 
   const interval = setInterval(() => {
-    progress += 2;
-    progressBar.style.width = progress + "%";
 
-    ipText.textContent = generateIP();
-    pingText.textContent = Math.floor(Math.random() * 40 + 10) + " ms";
+    progress++;
+    progressText.textContent = progress + "%";
+
+    circle.style.background =
+      `conic-gradient(#22c55e ${progress * 3.6}deg, #1f2937 0deg)`;
+
+    ipText.textContent = randomIP();
+    pingText.textContent = Math.floor(Math.random() * 30 + 10) + " ms";
 
     if (progress >= 100) {
       clearInterval(interval);
 
-      statusText.textContent = "Connected";
-      statusDot.style.background = "#22c55e";
+      statusTitle.textContent = "Подключено";
+      statusSub.textContent = "Соединение стабильно";
+      indicator.style.background = "#22c55e";
 
       setTimeout(() => {
         app.style.display = "none";
         loveScreen.classList.remove("hidden");
-      }, 1200);
+      }, 1500);
     }
 
-  }, 50);
+  }, 30);
 
 });
 
-function generateIP() {
-  return `${rand()}.${rand()}.${rand()}.${rand()}`;
+function randomIP() {
+  return `${r()}.${r()}.${r()}.${r()}`;
 }
 
-function rand() {
+function r() {
   return Math.floor(Math.random() * 255);
 }
